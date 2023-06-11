12869e8207fee6ecea267c0f097d57b8358f1a41
311609 <94932711+311609@users.noreply.github.com>
2023-06-10 22:34:15 -0600

    se realiza la implementación de funciones

--- Tareas.js
+++ Tareas.js
@@ -1,75 +1,112 @@
-const readline = require('readline-sync');
+const readline = require('readline-promise').default;
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 // Lista de tareas
 const tasks = [];
 
 // Función para añadir una tarea
 function addTask() {
-  const indicator = readline.question('Indicador: ');
-  const description = readline.question('Descripción: ');
-  const state = 'incompleta';
+  return new Promise((resolve, _reject) => {
+    rl.questionAsync('Indicador: ')
+      .then(indicator => {
+        return rl.questionAsync('Descripción: ')
+          .then(description => {
+            const state = 'incompleta';
 
-  const task = {
-    indicator,
-    description,
-    state
-  };
+            const task = {
+              indicator,
+              description,
+              state,
+            };
 
-  tasks.push(task);
-  console.log('Tarea añadida con éxito.');
+            tasks.push(task);
+            resolve('Tarea añadida con éxito.');
+          });
+      });
+  });
 }
 
 // Función para eliminar una tarea
 function removeTask() {
-  const index = readline.question('Índice de la tarea a eliminar: ');
-  
-  if (index >= 0 && index < tasks.length) {
-    tasks.splice(index, 1);
-    console.log('Tarea eliminada con éxito.');
-  } else {
-    console.log('Índice de tarea no válido.');
-  }
+  return new Promise((resolve, reject) => {
+    rl.questionAsync('Índice de la tarea a eliminar: ')
+      .then(index => {
+        index = parseInt(index);
+
+        if (index >= 0 && index < tasks.length) {
+          tasks.splice(index, 1);
+          resolve('Tarea eliminada con éxito.');
+        } else {
+          reject('Índice de tarea no válido.');
+        }
+      });
+  });
 }
 
 // Función para marcar una tarea como completada
 function completeTask() {
-  const index = readline.question('Índice de la tarea a marcar como completada: ');
-  
-  if (index >= 0 && index < tasks.length) {
-    tasks[index].state = 'completada';
-    console.log('Tarea marcada como completada.');
-  } else {
-    console.log('Índice de tarea no válido.');
-  }
+  return new Promise((resolve, reject) => {
+    rl.questionAsync('Índice de la tarea a marcar como completada: ')
+      .then(index => {
+        index = parseInt(index);
+
+        if (index >= 0 && index < tasks.length) {
+          tasks[index].state = 'completada';
+          resolve('Tarea marcada como completada.');
+        } else {
+          reject('Índice de tarea no válido.');
+        }
+      });
+  });
 }
 
 // Función principal
-function main() {
-  while (true) {
-    console.log('\n--- Lista de Tareas ---');
-    console.log('1. Añadir tarea');
-    console.log('2. Eliminar tarea');
-    console.log('3. Marcar tarea como completada');
-    console.log('4. Salir');
+async function main() {
+  let exit = false; // Variable para controlar la salida del bucle
 
-    const option = readline.question('Elige una opción: ');
+  console.log('\n--- Lista de Tareas ---');
+  console.log('1. Añadir tarea');
+  console.log('2. Eliminar tarea');
+  console.log('3. Marcar tarea como completada');
+  console.log('4. Salir');
 
-    switch (option) {
+  rl.on('line', async (input) => {
+    switch (input) {
       case '1':
-        addTask();
+        try {
+          const addResult = await addTask();
+          console.log(addResult);
+        } catch (error) {
+          console.error('Error al añadir tarea:', error);
+        }
         break;
       case '2':
-        removeTask();
+        removeTask()
+          .then((removeResult) => {
+            console.log(removeResult);
+          })
+          .catch((error) => {
+            console.error('Error al eliminar tarea:', error);
+          });
         break;
       case '3':
-        completeTask();
+        try {
+          const completeResult = await completeTask();
+          console.log(completeResult);
+        } catch (error) {
+          console.error('Error al marcar tarea como completada:', error);
+        }
         break;
       case '4':
-        return; // Salir del programa
+        rl.close();
+        break;
       default:
         console.log('Opción no válida. Inténtalo de nuevo.');
     }
-  }
+  });
 }
 
 // Ejecutar el programa principal
